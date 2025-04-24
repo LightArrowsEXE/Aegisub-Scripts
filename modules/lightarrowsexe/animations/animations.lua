de2a3c28ccea0a0785ec6a60106cf7254effaa9f
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2025-04-24 16:02:13 +0200

    [animations] Export drawings

--- modules/lightarrowsexe/animations/animations.lua
+++ modules/lightarrowsexe/animations/animations.lua
@@ -6,20 +6,38 @@
     Author: LightArrowsEXE <https://github.com/LightArrowsEXE>
 ]]
 
+-- First load all required modules
+local frame = require("lightarrowsexe.animations.utils.frame")
+
+local sparkles = require("lightarrowsexe.animations.drawings.sparkles")
+
+local animal = require("lightarrowsexe.animations.drawings.animal")
+
+local pixelart_heart = require("lightarrowsexe.animations.drawings.pixelart.heart")
+local pixelart_rings = require("lightarrowsexe.animations.drawings.pixelart.rings")
+local pixelart_sparkburst = require("lightarrowsexe.animations.drawings.pixelart.sparkburst")
+local pixelart_plasma = require("lightarrowsexe.animations.drawings.pixelart.plasma")
+
+
+-- Then create the animations table
 local animations = {
     version = "0.0.1",
+    frame = frame,
+    shapes = {
+        -- common shapes
+        starburst = sparkles.starburst,
 
-    -- Import utility modules
-    frame = require("utils.frame")
-}
+        -- specific shapes
+        animal = animal,
 
--- Import shape definitions
-animations.shapes = {
-    -- common shapes
-    sparkles = require("drawings.sparkles"),
-
-    -- specific shapes
-    animal = require("drawings.animal"),
+        -- pixel art
+        pixelart = {
+            heart = pixelart.heart,
+            rings = pixelart.rings,
+            sparkburst = pixelart.sparkburst,
+            plasma = pixelart.plasma
+        }
+    }
 }
 
 return animations
